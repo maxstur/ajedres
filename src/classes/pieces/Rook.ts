2441a8ede9c76c97e426b6bcb7662ef12490af0b
Matias Bontempo <matias.bontempo@gmail.com>
2021-04-19 23:25:20 -0300

    feat(rook): availableMovements

--- src/classes/pieces/Rook.ts
+++ src/classes/pieces/Rook.ts
@@ -1,9 +1,17 @@
 import Piece from '../Piece';
+import Cell from '../Cell';
 import { PieceType } from '../../types';
 
 class Rook extends Piece {
   constructor(color) {
     super(color, ['♜', '♖'], PieceType.rook);
+  }
+
+  availableMovements(position: [number, number], boardMatrix: Cell[][]) {
+    this.checkDirection(position, [0, -1], boardMatrix);
+    this.checkDirection(position, [1, 0], boardMatrix);
+    this.checkDirection(position, [0, 1], boardMatrix);
+    this.checkDirection(position, [-1, 0], boardMatrix);
   }
 }
 
