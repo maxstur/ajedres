39b00e5414b431863c705b2a0ba73971f4c9b0d0
Matias Bontempo <matias.bontempo@gmail.com>
2021-04-19 23:29:38 -0300

    feat(queen): availableMovements

--- src/classes/pieces/Queen.ts
+++ src/classes/pieces/Queen.ts
@@ -1,9 +1,25 @@
 import Piece from '../Piece';
+import Cell from '../Cell';
 import { PieceType } from '../../types';
 
 class Queen extends Piece {
   constructor(color) {
     super(color, ['♛', '♕'], PieceType.queen);
+  }
+
+  availableMovements(position: [number, number], boardMatrix: Cell[][]) {
+    const directions = [
+      [1, 1],
+      [-1, 1],
+      [1, -1],
+      [-1, -1],
+      [0, -1],
+      [1, 0],
+      [0, 1],
+      [-1, 0],
+    ];
+
+    directions.forEach((dir) => this.checkDirection(position, dir, boardMatrix));
   }
 }
 
