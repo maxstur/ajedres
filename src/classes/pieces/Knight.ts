102b5b1673e87940164d4a55c00680fd04b7f923
Matias Bontempo <matias.bontempo@gmail.com>
2021-04-19 23:12:51 -0300

    feat(knight): availableMovements

--- src/classes/pieces/Knight.ts
+++ src/classes/pieces/Knight.ts
@@ -1,9 +1,30 @@
 import Piece from '../Piece';
+import Cell from '../Cell';
 import { PieceType } from '../../types';
 
 class Knight extends Piece {
   constructor(color) {
     super(color, ['♞', '♘'], PieceType.knight);
+  }
+
+  availableMovements(position: [number, number], boardMatrix: Cell[][]) {
+    const [x, y] = position;
+
+    const possibleMovements: [number, number][] = [
+      [x - 1, y - 2],
+      [x + 1, y - 2],
+      [x + 2, y - 1],
+      [x + 2, y + 1],
+      [x + 1, y + 2],
+      [x - 1, y + 2],
+      [x - 2, y + 1],
+      [x - 2, y - 1],
+    ];
+
+    possibleMovements.forEach((pm) => {
+      const cell = this.getCellFromCoords(pm, boardMatrix);
+      if (cell && !(cell.piece && cell.piece.color === this.color)) cell.setAvailableMovement(true);
+    });
   }
 }
 
