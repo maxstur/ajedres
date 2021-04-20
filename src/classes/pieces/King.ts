f76dbb634d31effc7075c6181c1c592b66232c5d
Matias Bontempo <matias.bontempo@gmail.com>
2021-04-19 23:33:24 -0300

    feat(king): availableMovements

--- src/classes/pieces/King.ts
+++ src/classes/pieces/King.ts
@@ -1,9 +1,30 @@
 import Piece from '../Piece';
+import Cell from '../Cell';
 import { PieceType } from '../../types';
 
 class King extends Piece {
   constructor(color) {
     super(color, ['♚', '♔'], PieceType.king);
+  }
+
+  availableMovements(position: [number, number], boardMatrix: Cell[][]) {
+    const [x, y] = position;
+    const directions = [
+      [1, 1],
+      [-1, 1],
+      [1, -1],
+      [-1, -1],
+      [0, -1],
+      [1, 0],
+      [0, 1],
+      [-1, 0],
+    ];
+
+    directions.forEach((dir) => {
+      const [xDir, yDir] = dir;
+      const cell = this.getCellFromCoords([x + (1 * xDir), y + (1 * yDir)], boardMatrix);
+      if (cell && !(cell.piece && cell.piece.color === this.color)) cell.setAvailableMovement(true);
+    });
   }
 }
 
