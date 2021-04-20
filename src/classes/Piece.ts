094117b9f13884912d0c79ff42f9e73149965fb1
Matias Bontempo <matias.bontempo@gmail.com>
2021-04-19 23:25:06 -0300

    refactor(piece): checkDirection method

--- src/classes/Piece.ts
+++ src/classes/Piece.ts
@@ -21,6 +21,18 @@ class Piece {
     return cell;
   }
 
+  checkDirection(position, direction, boardMatrix) {
+    const [x, y] = position;
+    const [xDir, yDir] = direction;
+    for (let i = 1; i <= boardMatrix.length; i += 1) {
+      const cell = this.getCellFromCoords([x + (i * xDir), y + (i * yDir)], boardMatrix);
+      if (!cell) break;
+      if (cell.piece && cell.piece.color === this.color) break;
+      cell.setAvailableMovement(true);
+      if (cell.piece) break;
+    }
+  }
+
   // eslint-disable-next-line no-unused-vars
   availableMovements(position: [number, number], boardMatrix: Cell[][]) {
     throw new Error(`Missing availableMovements in ${this.type}`);
--- src/classes/pieces/Bishop.ts
+++ src/classes/pieces/Bishop.ts
@@ -7,29 +7,18 @@ class Bishop extends Piece {
     super(color, ['♝', '♗'], PieceType.bishop);
   }
 
-  checkDiagonal(position, movement, boardMatrix) {
-    const [x, y] = position;
-    const [xMov, yMov] = movement;
-    for (let i = 1; i <= boardMatrix.length; i += 1) {
-      const cell = this.getCellFromCoords([x + (i * xMov), y + (i * yMov)], boardMatrix);
-      if (!cell) break;
-      if (cell.piece && cell.piece.color === this.color) break;
-      cell.setAvailableMovement(true);
-      if (cell.piece) break;
-    }
-  }
   availableMovements(position: [number, number], boardMatrix: Cell[][]) {
     // Down Right
-    this.checkDiagonal(position, [1, 1], boardMatrix);
+    this.checkDirection(position, [1, 1], boardMatrix);
 
     // Down Left
-    this.checkDiagonal(position, [-1, 1], boardMatrix);
+    this.checkDirection(position, [-1, 1], boardMatrix);
 
     // Up Right
-    this.checkDiagonal(position, [1, -1], boardMatrix);
+    this.checkDirection(position, [1, -1], boardMatrix);
 
     // Up Left
-    this.checkDiagonal(position, [-1, -1], boardMatrix);
+    this.checkDirection(position, [-1, -1], boardMatrix);
   }
 }
 
